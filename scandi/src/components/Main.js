7cff7f48b72177affa4f215d86f8097f743d8f83
seadmasetic <maseticsead@gmail.com>
2022-10-29 11:57:20 +0200

    Header create

---
+++ scandi/src/components/Main.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+import classes from './Main.module.css';
+
+class Main extends Component{
+    render() {
+        return(
+            <div>
+                <h1></h1>
+            </div>
+        )
+    }
+}
+
+export default Main;
